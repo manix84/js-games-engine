948d10b31a143616d18b6993b96f35e27accf055
Rob Taylor <manix84@gmail.com>
2012-06-04 18:33:31 +0100

    Adding first draft of Fullscreen controller.

---
+++ Fullscreen.js
@@ -0,0 +1,53 @@
+define(function () {
+
+    /**
+     * Games Engine v1.0 - Fullscreen
+     * @author  Rob Taylor [manix84@gmail.com]
+     * @param {Function} element - User defined element to full screen
+     * @param {Object} options - Options for the current ticker.
+     * @return {Object} Ticker parent object
+     * @constructor
+     */
+    var Fullscreen = function (element, options) {
+
+        return this;
+    };
+
+    Fullscreen.prototype = {
+
+        /**
+         * Self perpetuating tick method. Can only be stopped with the stop command. Designed to stop callback stacking.
+         * @private
+         * @todo Need to look into adding a skip-frame ability.
+         */
+        toggle: function () {
+
+            if ((document.fullScreenElement && document.fullScreenElement !== null) ||
+                    (!document.mozFullScreen && !document.webkitIsFullScreen)) {
+            } else {
+            }
+        },
+
+        enter: function () {
+            var de = document.documentElement,
+                func = de.requestFullScreen ||
+                    de.mozRequestFullScreen ||
+                    de.webkitRequestFullScreen ||
+                    function () {};
+
+            func(Element.ALLOW_KEYBOARD_INPUT);
+        },
+
+        exit: function () {
+            if (document.cancelFullScreen) {
+                document.cancelFullScreen();
+            } else if (document.mozCancelFullScreen) {
+                document.mozCancelFullScreen();
+            } else if (document.webkitCancelFullScreen) {
+                document.webkitCancelFullScreen();
+            }
+        }
+    };
+
+    return Fullscreen;
+});
